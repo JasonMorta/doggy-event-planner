336b8d2b60da82711539df571016a8820f78bee4
JasonMorta <jason.morta23@gmail.com>
2022-08-11 14:15:47 +0200

    updates

--- frontend/src/components/footer/About.jsx
+++ frontend/src/components/footer/About.jsx
@@ -19,9 +19,9 @@ export default function About() {
        ))}
      </div>
       <p>
-        Some dogs need socialization, others may need a calm fiend,
-        or evn just one that's the same size.
-        Whether thy're tiny, wild, anxious or old, they deserve a time and space where they can feel
+      Some dogs need socialization, others may need a calm fiend,
+        or even just one that's the same size.
+        Whether they’re tiny, wild, anxious or old, they deserve a time and space where they can feel
         secure and free with other dogs of a similar size and/or energy level.
       </p>
       <p>A safe space for your furbabies to plan meetups, Socialize, and Play.</p>
